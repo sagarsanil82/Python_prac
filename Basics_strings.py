c6d58c781dd8cf88104645e45466989b9a619785
Sagar Sanil <sagarsanil82@gmail.com>
2025-10-25 10:06:37 +0530

    String basics

---
+++ Basics_strings.py
@@ -0,0 +1,34 @@
+# number=list(map(int,input("Enter the number").split()))
+
+
+
+# print(max(number))
+# print(min(number))
+
+#################Day 2################:
+
+x = 10
+y = 3.5
+z = 2 + 5j
+
+print(type(x),type(y),type(z))
+
+
+##Strings##
+
+name="Sagar is a python genius"
+
+print(name[1:3])
+print(name.lower())
+print(len(name))
+print(name.split())
+
+words = ['Python', 'is', 'fun']
+
+sentence=" ".join(words)
+print(sentence)
+
+print(name.upper())
+print(name.replace("python","Python"))
+
+
